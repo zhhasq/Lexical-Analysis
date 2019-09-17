fbc98d7a309152ec75e12dd7112b5f341c8fc3cc
zhongs <zhongs@unm.edu>
2019-09-17 16:15:45 -0600

    first commit

---
+++ src/main/java/edu/unm/lexer/App.java
@@ -0,0 +1,14 @@
+package edu.unm.lexer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
